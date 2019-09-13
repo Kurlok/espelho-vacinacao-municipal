6747371a22e21da3dfb6f71e2f6d8d9f6cddacef
Kurlok <felipe.augum@gmail.com>
2019-09-13 16:55:09 -0300

    Começo de gráficos

--- app/Http/Controllers/RelatoriosController.php
+++ app/Http/Controllers/RelatoriosController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Vacina;
 use Illuminate\Http\Request;
 
 class RelatoriosController extends Controller
 {
     public function index()
     {
+        $listaVacina = Vacina::all();
         return view(
             'relatorios/relatorios',
+            [
+                'listaVacina' => $listaVacina,
+            ]
         );
     }}
