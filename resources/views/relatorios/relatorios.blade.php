524824b62613abbc4d6cea83e8a89c604fd1fb3e
Kurlok <felipe.augum@gmail.com>
2019-09-13 14:45:43 -0300

    Começo da implementação dos gráficos

--- resources/views/relatorios/relatorios.blade.php
+++ resources/views/relatorios/relatorios.blade.php
@@ -1,30 +1,37 @@
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+@endpush
+
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
-<script>
-    new Chart(document.getElementById("bar-chart"), {
-    type: 'bar',
-    data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
-    }
-});
+    <div class="col-md-3">
+        <h2><i class="fas fa-chart-bar"></i> Relatórios</a></h2>
+    </div>
+    <canvas id="myChart" width="50vh" height="20vh"></canvas>
+    <script>
+        new Chart(document.getElementById("myChart"), {
+            type: 'bar',
+            data: {
+                labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+                datasets: [{
+                    label: "Population (millions)",
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                    data: [2478, 5267, 734, 784, 433]
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: 'Predicted world population (millions) in 2050'
+                }
+            }
+        });
     </script>
-<canvas id="bar-chart" width="800" height="450"></canvas>
 
 </div>
 @endsection
