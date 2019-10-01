941e10c1af810abfadfdc60eaa54b850df52796d
Kurlok <felipe.augum@gmail.com>
2019-10-01 10:23:59 -0300

    Começando a tela de relatórios

--- resources/views/relatorios/relatorios.blade.php
+++ resources/views/relatorios/relatorios.blade.php
@@ -7,6 +7,38 @@
             <h2><i class="fas fa-file-export"></i> Relatórios</a></h2>
         </div>
     </div>
-  
+
+    <div id="list" class="row">
+        <div class="table-responsive col-md-12">
+            <table class="table table-striped" cellspacing="0" cellpadding="0">
+                <thead>
+                    <tr>
+                        <th class="col-md-10">Exportar</th>
+
+                        <th class="actions col-md-2">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Todas as vacinas</td>
+
+
+                        <td class="actions">
+
+                            <form action="" method="post">
+                                {{csrf_field()}}
+                                <button type="submit" class="btn btn-success btn-xs">
+                                    <i class="far fa-file-excel"></i> Excel
+                                </button>
+                            </form>
+                           
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+        </div>
+    </div>
 </div>
 @endsection
